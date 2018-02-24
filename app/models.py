b78cd00cdc202bb911f13e5ac42bc9e86da9a856
Mabu <manailengmj@gmail.com>
2018-02-24 21:52:23 +0200

    models

--- app/models.py
+++ app/models.py
@@ -14,16 +14,16 @@ class Audits(models.Model):
 
 class Municipalities(models.Model):
     area = models.CharField(max_length=9)
-    category = models.Chafield(max_length=2)
-    demarcation_code = models.Charfield(max_length=6)
-    fax_number = models.Charfield(max_length=10)
-    name = models.Charfield(max_length=50)
-    phone_number = models.Charfield(max_length=10)
-    postal_address_1 = models.Charfield(max_length=50)
-    postal_address_2 = models.Charfield(max_length=50)
-    postal_code  = models.Charfield(max_length=4)
+    category = models.CharField(max_length=2)
+    demarcation_code = models.CharField(max_length=6)
+    fax_number = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+    postal_address_1 = models.CharField(max_length=50)
+    postal_address_2 = models.CharField(max_length=50)
+    postal_code = models.Charfield(max_length=4)
     province_name = models.Charfield(max_length=20)
-    street_address_1  = models.Charfield(max_length=50)
+    street_address_1 = models.Charfield(max_length=50)
     street_address_2 = models.Charfield(max_length=50)
     street_address_3 = models.Charfield(max_length=30)
     website = models.Charfield(max_length=30)
@@ -37,3 +37,52 @@ class Officials(models.Model):
     fax_number = models.Charfield(max_length=10)
     phone_number = models.Charfield(max_length=10)
     demarcation_code = models.Charfield(max_length=6)
+
+
+class Maintenance(models.Model):
+    demarcation_code
+    amount
+    amount_type
+    financial_period
+    financial_year_end
+    item_label
+
+
+class CashFlow(models.Model):
+    demarcation_code
+    amount
+    amount_type
+    financial_year_end
+    item_label
+
+
+class BalanceSheet(models.Model):
+    amount
+    demarcation_code
+    demarcation_label
+    financial_year_end
+    item_label
+
+
+class Capital(models.Model):
+    amount_type
+    financial_year_end
+    item_label
+    demarcation_code
+    demarcation_label
+
+
+class IncomeExpense(models.Model):
+    amount_type
+    financial_year_end
+    item_label
+    demarcation_code
+    demarcation_label
+
+
+class WastefulExpenditure(models.Model):
+    amount
+    demarcation_code
+    demarcation_label
+    financial_year_end
+    item_label
