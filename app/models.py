4b819beab4d26580f86c110fe4535c69da385674
Mabu <manailengmj@gmail.com>
2018-02-24 17:06:35 +0200

    models

--- app/models.py
+++ app/models.py
@@ -10,3 +10,30 @@ class Audits(models.Model):
     financial_year_end = models.CharField(max_length=4)
     opinion = models.CharField(max_length=100)
     opinion_report_url = models.CharField(max_length=100)
+
+
+class Municipalities(models.Model):
+    area
+    category
+    demarcation_code
+    fax_number
+    name
+    phone_number
+    postal_address_1
+    postal_address_2
+    postal_code
+    province_name
+    street_address_1
+    street_address_2
+    street_address_3
+    website
+
+
+class Officials(models.Model):
+    title
+    role
+    name
+    email_address
+    fax_number
+    phone_number
+    demarcation_code
