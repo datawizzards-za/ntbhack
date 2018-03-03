69164ad82d6c7fd8e198abf2b77cca4f32f04560
Mabu <manailengmj@gmail.com>
2018-03-03 02:13:12 +0200

    working on crossfilter

--- app/static/scripts/kpi_dashbords.js
+++ app/static/scripts/kpi_dashbords.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    console.log("Try getting API data");
     queue()
         .defer(d3.json, "/app/api/get_data/")
         .awaitAll(handleData);
@@ -8,6 +9,7 @@ $(document).ready(function () {
     }
 
     function graphKPIDashboard(recordsJson) {
+        console.log("Inside graphing func");
         //Clean data
         var records = [];
         //var dateFormat = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ");
@@ -15,22 +17,60 @@ $(document).ready(function () {
 
 
         recordsJson.forEach(function (d) {
+            console.log("Entering json loop");
+            audits_len = d['audits'].length
+            wasteful_len = d['wasteful'].length
+            maintenance_len = d['maintenance'].length
+            balance_sheets = d['maintenance'].length
+            maintenance_len = d['maintenance'].length
+            maintenance_len = d['maintenance'].length
+            maintenance_len = d['maintenance'].length
+            maintenance_len = d['maintenance'].length
 
-            for (var i = 0; i < d['audits'].length; i++) {
+            var length = audits_len
+            if (wasteful_len > length) {
+                length = wasteful_len
+            }
+            if (maintenance_len > length) {
+                length = maintenance_len
+            }
+
+            for (var i = 0; i < length; i++) {
+                console.log("Entering length loop");
                 tmp = {}
                 tmp['category'] = d['category']
                 tmp['name'] = d['name']
-                tmp['audits_fy'] = d['audits'][i]['financial_year_end']
-                tmp['audit_opinion'] = d['audits'][i]['opinion']
-                tmp['wasteful_fy'] = d['wasteful'][i]['financial_year_end']
-                tmp['wasteful_amount'] = Number(d['wasteful'][i]['amount'])
-                tmp['wasteful_name'] = d['wasteful'][i]['item_label']
-
-                //console.log()
+                if (i < audits_len) {
+                    tmp['audits_fy'] = d['audits'][i]['financial_year_end']
+                    tmp['audit_opinion'] = d['audits'][i]['opinion']
+                } else {
+                    tmp['audits_fy'] = 'None'
+                    tmp['audit_opinion'] = 'None'
+                }
+                if (i < wasteful_len) {
+                    tmp['wasteful_fy'] = d['wasteful'][i]['financial_year_end']
+                    tmp['wasteful_amount'] = Number(d['wasteful'][i]['amount'])
+                    tmp['wasteful_name'] = d['wasteful'][i]['item_label']
+                } else {
+                    tmp['wasteful_fy'] = 'None'
+                    tmp['wasteful_amount'] = 'None'
+                    tmp['wasteful_name'] = 'None'
+                }
+                if (i < maintenance_len) {
+                    tmp['maintenance_fy'] = d['maintenance'][i]['financial_year_end']
+                    tmp['maintenance_amount'] = Number(d['maintenance'][i]['amount'])
+                    tmp['maintenance_name'] = d['maintenance'][i]['item_label']
+                    tmp['maintenance_type'] = d['maintenance'][i]['amount_type']
+                } else {
+                    tmp['maintenance_fy'] = 'None'
+                    tmp['maintenance_amount'] = 'None'
+                    tmp['maintenance_name'] = 'None'
+                    tmp['maintenance_type'] = 'None'
+                }
 
                 records.push(tmp);
             }
-
+            console.log("Leaving len loop");
 
             /*
             tmp["date_submitted"] = dateFormat.parse(d["date_submitted"]);
@@ -45,14 +85,17 @@ $(document).ready(function () {
             records.push(tmp);
             }*/
         });
+        console.log("Leaving json loop");
 
         //console.log(records);
 
+        console.log("Starting with crossfilter")
         //Create a Crossfilter instance
         var ndx = crossfilter(records);
 
         var categoryDim = ndx.dimension(function (d) { return d["category"]; });
         var categoryGroup = categoryDim.group();
+
         var charts_height = 150
         dc.pieChart("#category-chart")
             .width(200)
@@ -62,14 +105,22 @@ $(document).ready(function () {
             .legend(dc.legend().x(210).y(20))
             .innerRadius(20);
 
-        var auditsFYDim = ndx.dimension(function (d) { return d["audits_fy"]; });
-        var auditsFYGroup = auditsFYDim.group();
-        dc.barChart("#audits-chart")
+        var maintenanceFYDim = ndx.dimension(function (d) { return d["maintenance_fy"]; });
+        var maintenanceFYGroup = maintenanceFYDim.group();
+        var filteredMaintenanceGroup = {
+            all: function () {
+                return maintenanceFYGroup.top(Infinity).filter(function (d) {
+                    //console.log("#####", d.key);
+                    return d.key !== 'None';
+                });
+            }
+        }
+        dc.barChart("#maintenance-chart")
             .width(200)
             .height(charts_height)
             //.margins({ top: 10, right: 50, bottom: 40, left: 40 })
-            .dimension(auditsFYDim)
-            .group(auditsFYGroup)
+            .dimension(maintenanceFYDim)
+            .group(filteredMaintenanceGroup) //auditsFYGroup)
             .controlsUseVisibility(true)
             .transitionDuration(500)
             .x(d3.scale.ordinal().domain(["2011", "2012", "2013", "2014", "2015", "2016"]))
@@ -79,29 +130,59 @@ $(document).ready(function () {
             .barPadding(0.1)
             .yAxis().ticks(5);
 
+
+        var auditsFYDim = ndx.dimension(function (d) { return d["audits_fy"]; });
+        var auditsFYGroup = auditsFYDim.group();
+        var filteredAuditsGroup = {
+            all: function () {
+                return auditsFYGroup.top(Infinity).filter(function (d) {
+                    //console.log("#####", d.key);
+                    return d.key !== 'None';
+                });
+            }
+        }
+        dc.barChart("#audits-chart")
+            .width(200)
+            .height(charts_height)
+            //.margins({ top: 10, right: 50, bottom: 40, left: 40 })
+            .dimension(auditsFYDim)
+            .group(filteredAuditsGroup) //auditsFYGroup)
+            .controlsUseVisibility(true)
+            .transitionDuration(500)
+            .x(d3.scale.ordinal().domain(["2011", "2012", "2013", "2014", "2015", "2016"]))
+            .xUnits(dc.units.ordinal)
+            //.xAxisLabel("Year")
+            //.yAxisLabel("Number of audits")
+            .barPadding(0.1)
+            .yAxis().ticks(5);
+
+
         var wastefulFYDim = ndx.dimension(function (d) { return d["wasteful_fy"]; });
         //var wastefulamountDim = ndx.dimension(function (d) { return d["wasteful_amount"]; });
         var wastefulFYGroup = wastefulFYDim.group().reduceSum(function (d) { return d.wasteful_amount });
+        var filteredWastefulGroup = {
+            all: function () {
+                return wastefulFYGroup.top(Infinity).filter(function (d) {
+                    return d.key !== 'None';
+                });
+            }
+        }
         dc.barChart("#wasteful-chart")
             .width(170)
             .height(charts_height)
-            //.margins({ top: 10, right: 50, bottom: 40, left: 40 })
             .dimension(wastefulFYDim)
-            .group(wastefulFYGroup)
+            .group(filteredWastefulGroup)
             .controlsUseVisibility(true)
             .transitionDuration(500)
             .x(d3.scale.ordinal().domain(["2012", "2013", "2014", "2015"]))
             .xUnits(dc.units.ordinal)
-            //.yAxis()
             .barPadding(0.1)
             .yAxis().ticks(5).tickFormat(d3.format('.1s'));
-        //.xAxisLabel("Year")
-        //.yAxisLabel("Number of audits");
-
 
         dc.renderAll();
+        console.log("Done with crossfilter")
 
-        print_filter(wastefulFYGroup);
+        console.log(filteredAuditsGroup);
 
         /*/Define Dimensions
         var submittedDim = ndx.dimension(function (d) { return d["date_submitted"]; });
